22a67f22f7b5e3d3f021d965c288aa88fcad4b87
c mulligan <gateless@protonmail.com>
2019-05-25 22:05:09 -0400

    first draft

---
+++ main.js
@@ -0,0 +1,51 @@
+const player_frame = document.getElementById('player-frame')
+const cont = document.getElementById('cont')
+
+const player_factory = () => {
+  const init = (init) => ({get:()=>init,set:(next)=>init=next});
+  const state = init(false);
+
+  const player = document.createElement('audio');
+  player.src = 'http://stream.wcrsfm.org:8000/wcrsfm_a';
+
+  const info = document.createElement('text');
+  info.textContent = 'WCRS';
+
+  const button = document.createElement('div');
+  button.style.backgroundImage = 'url(assets/material_design_play.svg)';
+  button.style.height = '80%';
+  button.style.width = '20%';
+  button.style.backgroundRepeat = 'no-repeat';
+  button.style.backgroundSize = 'contain';
+
+  const load = (url,tag) => (player.src = url) && (info.textContent = tag);
+  const play = () => {player.play(); 
+         (button.style.backgroundImage = 'url(assets/material_design_pause.svg)');
+         state.set(true);}
+  const pause = () => {player.pause(); 
+         (button.style.backgroundImage = 'url(assets/material_design_play.svg)');
+         state.set(false);}
+
+  button.addEventListener('click',()=>(state.get()) ? pause() : play())
+
+  return ({load,play,pause,button,info})
+} 
+
+const player = player_factory()
+
+player_frame.append(player.button);
+player_frame.append(player.info);
+
+const audioListElement = ({text,src}) => {
+  const item = document.createElement('text')
+  item.textContent = text
+  item.addEventListener('click',()=>player.load(src,text) && player.play())
+  return item
+}
+
+const createAudioList = (obj) => {
+  cont.append(audioListElement(obj))
+}
+
+[{text:'WCRS',src:'http://stream.wcrsfm.org:8000/wcrsfm_a'},{text:'Liberation',src:'assets/505-Liberation.mp3'},{text:'Fresh Bitches',src:'assets/Timbral-Fresh_Bitches.mp3'}].map(createAudioList);
+
